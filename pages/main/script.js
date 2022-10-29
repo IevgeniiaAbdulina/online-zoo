5f5ee2b54594d122b0ab76e85bebc678e8d2daba
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2022-10-29 14:29:42 +0200

    refactor: change the structure of the Header navigation links

---
+++ pages/main/script.js
@@ -0,0 +1,16 @@
+// Interactive navigation bar.
+document.addEventListener("click", navClicked);
+function navClicked(event) {
+    const li = event.target.closest("li");
+    if (li) {
+        const clickedMenuValue = li.getAttribute('value');
+        const menus = document.querySelectorAll(".nav-link");
+        const menuArr = Array.from(menus);
+
+        const simMenus = menuArr.filter(item => item.getAttribute('value') === clickedMenuValue);
+        const simMenusArr = Array.from(simMenus);
+
+        menuArr.forEach(element => element.classList.remove("current"));
+        simMenusArr.forEach(element => element.classList.add("current"));
+    }
+}
