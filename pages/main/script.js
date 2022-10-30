545f6196d56b22eb5ab570a12aa9dceadf8f5201
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2022-10-30 21:32:04 +0100

    feat: implement correct button Submit, depends on Form validation

--- pages/main/script.js
+++ pages/main/script.js
@@ -14,3 +14,28 @@ function navClicked(event) {
         simMenusArr.forEach(element => element.classList.add("current"));
     }
 }
+
+// The button submit should be in the mistake position. If the field email passes validation, it goes into the default state.
+
+const buttonSubmit = document.getElementById("submit");
+buttonSubmit.addEventListener("click", event => {
+    event.preventDefault();
+    if (input.validity.valid) alert("You are subscribed =)");
+});
+
+const updateButtonState = () => {
+    if (!input.validity.valid) {
+        buttonSubmit.classList.add("invalid");
+    } else {
+        buttonSubmit.classList.remove("invalid");
+    }
+}
+
+const input = document.getElementById("email");
+input.addEventListener("input", event =>{
+    updateButtonState();
+});
+
+input.addEventListener("focus", event =>{
+    updateButtonState();
+});
